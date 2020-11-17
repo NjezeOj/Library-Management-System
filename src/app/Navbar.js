64f53c91367f9461ac6b5f842c95b308ea3e9e1a
NjezeOj <47678586+NjezeOj@users.noreply.github.com>
2020-11-17 14:10:00 +0100

    Updated

---
+++ src/app/Navbar.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { Link } from "react-router-dom"
+
+
+export const Navbar = () => {
+    
+    return(
+        <nav class=" text-white h-full bg-teal-400 fixed top-0 left-0 overflow-x-hidden w-40">
+            <div id="sidenav" class="flex flex-col items-center">
+                <div class="pt-8">
+                    <ul>
+                        <li class="text-xl pb-8">
+                            <Link to="/">BlueTag Library</Link>
+                        </li>
+
+                        <li>
+                            <Link to="/registercategory">Register Category</Link>
+                        </li>
+                        <li>
+                            <Link>Register Book</Link>
+                        </li>
+                        <li>
+                            <Link>Lend Book</Link>
+                        </li>
+                        <li>
+                            <Link>Return Book</Link>
+                        </li>
+                        <li>
+                            <Link>Borrowed Book Log</Link>
+                        </li>
+                        <li>
+                            <Link>Book Status</Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>    
+    )
+}
---
+++ src/features/HomePage.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+
+export const HomePage = () => {
+    return (
+        <img class="background" src="https://images.unsplash.com/photo-1533285860212-c85e7140a408?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80" alt="libraryImage"/>
+    )
+}
